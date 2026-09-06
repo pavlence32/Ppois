cb39b2ef084624578f9c25a580c53f83b63ee085
pavlence <kozhevnikov052008@gmail.com>
2026-09-06 19:28:43 +0300

    создание файла заголовка

---
+++ lab1/kubik.h
@@ -0,0 +1,13 @@
+#define kubik.h
+#include <string>
+class Kubick
+{
+    char faces[6][3][3];
+    char fill_place(int index);
+    public:
+    Kubick();
+    void load_color(std::string s);
+    Kubick &turn(int a);
+    bool checkcolorplace();
+    void random_place();
+};
